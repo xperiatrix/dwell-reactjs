e424668c394cbad4bb08d5338704126039a7f05e
toureek <june.key.young@gmail.com>
2019-10-11 19:59:43 +0800

    Add Pull-To-Refresh for ListPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import '../node_modules/material-design-lite/material.min.css';
 import '../node_modules/material-design-lite/material.min.js';
 import '../node_modules/material-design-icons/iconfont/material-icons.css';
 import TableViewComponent from './components/TableViewComponent';
+import {PullToRefresh} from "react-js-pull-to-refresh";
+import {PullDownContent, ReleaseContent, RefreshContent} from "react-js-pull-to-refresh";
 
 class App extends React.Component {
   constructor(props) {
@@ -18,10 +20,19 @@ class App extends React.Component {
       uitableview: null,
     }
     this.invokeApi = this.invokeApi.bind(this);
+    this.onRefresh = this.onRefresh.bind(this);
   }
 
   componentDidMount() {
     this.invokeApi();
+    console.log('+++++++++++++++')
+  }
+
+  onRefresh() {
+    this.invokeApi();
+    return new Promise((resolve) => {
+      setTimeout(resolve, 2000);
+    });
   }
 
   invokeApi() {
@@ -75,9 +86,9 @@ class App extends React.Component {
 
             <div className="mdl-layout__tab-bar mdl-js-ripple-effect"
                  style={{backgroundColor: '#5D43DA'}} >
-                <a href="#fixed-tab-1" className="mdl-layout__tab is-active"><b>Content</b></a>
-                <a href="#fixed-tab-2" className="mdl-layout__tab"><b>Map</b></a>
-                <a href="#fixed-tab-3" className="mdl-layout__tab"><b>Search Result</b></a>
+                <a href="#fixed-tab-1" className="mdl-layout__tab is-active"><b>整组房源列表</b></a>
+                <a href="#fixed-tab-2" className="mdl-layout__tab"><b>地图找房源</b></a>
+                <a href="#fixed-tab-3" className="mdl-layout__tab"><b>搜索结果</b></a>
             </div>
           </header>
 
@@ -92,23 +103,32 @@ class App extends React.Component {
           </div>
 
           <main className="mdl-layout__content">
-              <section className="mdl-layout__tab-panel is-active" id="fixed-tab-1">
-              <div className="page-content">{this.state.uitableview}</div>
-              </section>
+                <section className="mdl-layout__tab-panel is-active" id="fixed-tab-1">
+                      <div className="pull-to-refresh">
+                          <PullToRefresh
+                              pullDownContent={<PullDownContent label="下拉刷新" background="none" />}
+                              releaseContent={<ReleaseContent label="释放刷新" background="none" />}
+                              refreshContent={<RefreshContent background="./res/img/tab-pager-view.png" />}
+                              pullDownThreshold={200}
+                              onRefresh={this.onRefresh}
+                              triggerHeight={100}>
+                              <div className="page-content">{this.state.uitableview}</div>
+                          </PullToRefresh>
+                      </div>
+                </section>
+
               <section className="mdl-layout__tab-panel" id="fixed-tab-2">
                 <div className="page-content">
-                  
                   <iframe className="mapView" src="https://maplab.amap.com/share/mapv/6e87616c893ffa79aa1a8c431f552c55"></iframe>
-        
-
                 </div>
               </section>
+
               <section className="mdl-layout__tab-panel" id="fixed-tab-3">
-                <div className="page-content">Search Result View</div>
+                <div className="page-content">搜索结果</div>
               </section>
           </main>
       </div>
-
+      
     ); 
   }
 }
